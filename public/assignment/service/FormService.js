bf12c7ae160c298df831da3f8a0a2d764c2ba81a
Yaxin <elfinhuangcat@gmail.com>
2015-10-20 18:09:20 -0400

    HW3: working on the services

---
+++ public/assignment/service/FormService.js
@@ -0,0 +1,66 @@
+"use strict";
+(function() {
+    angular
+        .module("FormBuilderApp")
+        .factory("FormService", FormService);
+    function FormService() {
+        /**
+         * Each item in the array 'forms' has these properties:
+         * @property: id
+         * @property: userid
+         * And other properties (since a form shape can change.)
+         * @type {Array}
+         */
+        var forms = [];
+
+        function createFormForUser(userid, form, callback) {
+            form.id = guid();
+            form.userid = userid;
+            forms.push(form);
+            callback(form);
+        }
+
+        function findAllFormsForUser(userid, callback) {
+            var resultForms = [];
+            for (var form in forms) {
+                if (form.userid == userid) {
+                    resultForms.push(form);
+                }
+            }
+            callback(resultForms);
+            return resultForms;
+        }
+
+        function deleteFormById(formid, callback) {
+            for (var i = 0; i < forms.length; ++i) {
+                if (forms[i].id == formid) {
+                    console.log("Form deleted. ID: " + formid);
+                    forms.splice(i, 1);
+                    break;
+                }
+            }
+            callback(forms);
+        }
+
+        function updateFormById(formid, newForm, callback) {
+            for (var form in forms) {
+                if (form.id == formid) {
+                    for (var property in newForm) {
+                        
+                    }
+                }
+            }
+        }
+
+        /** Function to generate Guid, from instructor Jose Annunziato.**/
+        function guid() {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+            }
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        }
+    }
+}
--- public/assignment/service/UserService.js
+++ public/assignment/service/UserService.js
@@ -5,6 +5,13 @@
         .factory("UserService", UserService);
 
     function UserService() {
+        /**
+         * Each item in the array 'users' has these properties:
+         * @property: id
+         * @property: username
+         * @property: password
+         * @type {Array}
+         */
         var users = [];
 
         var service = {
@@ -12,8 +19,62 @@
         };
         return service;
 
-        function findUserByUsernameAndPassword(username, password, function(){}) {
+        function findUserByUsernameAndPassword(username, password, callback) {
+            var user;
+            for (user in users) {
+                if (user.username == username & user.password == password) {
+                    callback("User found.");
+                    return user;
+                }
+            }
+            callback("User not found.");
+            return null;
+        }
 
+        function findAllUsers(callback) {
+            callback("All users found"); // Because we don't need a callback function to fetch data now.
+            return users;
+        }
+
+        function createUser(user, callback) {
+            // user object contains username and password
+            user.id = guid();
+            users.push(user);
+            callback(user);
+        }
+
+        function deleteUserById(id, callback) {
+            for (var i = 0; i < users.length; ++i) {
+                if (users[i].id == id) {
+                    console.log("User found. Delete user: " + users[i].id);
+                    users.splice(i, 1);
+                    break;
+                }
+            }
+            callback(users);
+        }
+
+        function updateUser(id, user, callback) {
+            for (var item in users) {
+                if (item.id == id) {
+                    item.username = user.username;
+                    item.password = user.password;
+                    callback(item);
+                    break;
+                }
+            }
+        }
+
+
+        /** Function to generate Guid, from instructor Jose Annunziato.**/
+        function guid() {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+            }
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
         }
     }
 })();
