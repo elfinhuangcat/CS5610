34d5cb675c059dc4dab179a6d7fc2d833af9b220
Yaxin <elfinhuangcat@gmail.com>
2015-12-09 16:09:47 -0500

    Project: try to fix the slider

--- public/project/client/views/index.views/slider.controller.js
+++ public/project/client/views/index.views/slider.controller.js
@@ -14,16 +14,16 @@
                 .findRecipeById("56689255502ba10f79eb74bf")
                 .then(function(recipe) {
                     vm.recipes.push(recipe);
-                });
-            RecipeService
-                .findRecipeById("5668934e502ba10f79eb74c1")
-                .then(function(recipe) {
-                    vm.recipes.push(recipe);
-                });
-            RecipeService
-                .findRecipeById("566893f5502ba10f79eb74c2")
-                .then(function(recipe) {
-                    vm.recipes.push(recipe);
+                    RecipeService
+                        .findRecipeById("5668934e502ba10f79eb74c1")
+                        .then(function(recipe) {
+                            vm.recipes.push(recipe);
+                            RecipeService
+                                .findRecipeById("566893f5502ba10f79eb74c2")
+                                .then(function(recipe) {
+                                    vm.recipes.push(recipe);
+                                });
+                        });
                 });
         }
         init();
